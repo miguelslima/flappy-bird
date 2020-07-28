f71dad23881c1ef4b1f0a9582a46216cfc2aae31
Miguel <miguelsousalima@hotmail.com>
2020-07-28 17:27:59 -0300

    Tela Inicial e troca tela

--- jogo.js
+++ jogo.js
@@ -87,14 +87,75 @@ const flappyBird = {
   }
 }
 
-function loop() {
-  flappyBird.atualiza();
+const messagemGetReady = {
+  spriteX: 134,
+  spriteY: 0,
+  largura: 174,
+  altura: 152,
+  x: (canvas.width / 2) - 174 / 2,
+  y: 50,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      messagemGetReady.spriteX, messagemGetReady.spriteY, //Sprite X, Sprite Y
+      messagemGetReady.largura, messagemGetReady.altura, // Tamanho do recorte na sprite
+      messagemGetReady.x, messagemGetReady.y,
+      messagemGetReady.largura, messagemGetReady.altura,
+    );
+  }
+}
 
-  background.desenha();
-  chao.desenha();
-  flappyBird.desenha();
+//
+// [TELAS]
+//
+
+let telaAtiva = {};
+
+function mudaParaTela(novaTela) {
+  telaAtiva = novaTela;
+}
+
+const Telas = {
+  INICIO: {
+    desenha() {
+      background.desenha();
+      chao.desenha();
+      messagemGetReady.desenha();
+      flappyBird.desenha();
+    },
+    click() {
+      mudaParaTela(Telas.JOGO)
+    },
+    atualiza() {
+
+    }
+  }
+};
+
+Telas.JOGO = {
+  desenha() {
+    background.desenha();
+    chao.desenha();
+    flappyBird.desenha();
+  },
+  atualiza () {
+    flappyBird.atualiza();
+  }
+}
+
+function loop() {
+
+  telaAtiva.desenha();
+  telaAtiva.atualiza();
 
   requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', function() {
+  if(telaAtiva.click) {
+    telaAtiva.click();
+  }
+})
+
+mudaParaTela(Telas.INICIO);
 loop();
